33a9b1ce185937795883df3873a479ce610727e4
phiweCode <thabo.s.mngoma@gmail.com>
2025-07-10 04:48:28 +0200

    implemented reset followers

--- SampleFolder/followersCount/followers_count.js
+++ SampleFolder/followersCount/followers_count.js
@@ -20,3 +20,7 @@ function displayCount()
     document.getElementById('countDisplay').innerHTML=count; 
 } 
 
+function resetFollowers () { 
+     document.getElementById('countDisplay').innerHTML= 0 
+     alert("Your Instagram followers have been reset!");
+}
